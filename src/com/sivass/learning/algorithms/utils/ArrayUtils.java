c800664c2006313a77c68c264470cb04f371b717
Siva <shekrunk@users.noreply.github.com>
2018-05-14 13:40:45 +0530

    Updated print array method to more generic type

--- src/com/sivass/learning/algorithms/utils/ArrayUtils.java
+++ src/com/sivass/learning/algorithms/utils/ArrayUtils.java
@@ -1,12 +1,31 @@
 package com.sivass.learning.algorithms.utils;
 
+import java.lang.reflect.Array;
+
 public class ArrayUtils {
-	public static void print(int[] array) {
+
+	public static <T> void print(T array) {
+		print(array, array.getClass().getComponentType());
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static <T> void print(T array, Class<? extends Object> class1) {
 		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<array.length;i++) {
-			sb.append(array[i]);
-			if(i!=array.length-1) {
-				sb.append(", ");
+		if(class1.isPrimitive()) {
+	        int length = Array.getLength(array);
+	        for (int i = 0; i < length; i++) {
+	        	sb.append(Array.get(array, i));
+	        	if(i!=length-1) {
+					sb.append(", ");
+				}
+	        }
+		} else {
+			T[] array1 = (T[]) array;
+			for(int i=0;i<array1.length;i++) {
+				sb.append(array1[i]);
+				if(i!=array1.length-1) {
+					sb.append(", ");
+				}
 			}
 		}
 		System.out.println(sb);
